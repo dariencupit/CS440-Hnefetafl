3dd3f26cd507411f660bd949d37c7ce368264c9f
Darien Cupit <dariencupit1@gmail.com>
2019-12-11 14:41:06 -0700

    Implemented skeleton code.

---
+++ client/src/main/java/client/GameAI.java
@@ -0,0 +1,35 @@
+package client;
+
+public class GameAI {
+	// Action
+	//		- An action is represented as an int array with a size of 4
+	//		The first two slots in the array represent from, and the last two
+	//		represent to in the form of y,x
+	// State
+	//		- State is represented as a double char array of characters
+	
+	public int[] Minimax_Decision(char[][] state) { // Implements Alpha-Beta
+		
+		return new int[0];
+	}
+	
+	public double Max_Value(char[][] state) {
+		
+		return 0.0;
+	}
+	
+	public double Min_Value(char[][] state) {
+		
+		return 0.0;
+	}
+	
+	public double Heuristic(char[][] state) {
+		
+		return 0.0;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
